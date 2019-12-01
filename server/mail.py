2614acc9b39dbcc807e9f6858e05199c4df961cc
patrick <trickmartini@gmail.com>
2019-12-01 02:23:52 -0300

    [ADD]Função de envio de email

--- server/mail.py
+++ server/mail.py
@@ -1,21 +1,23 @@
 import smtplib
+from email.message import EmailMessage
 
 class Mail:
     def __init__(self):
         pass
-    def sendMail(self, menssagem="um belo texto", destinatario="trickmartini@gmail.com"):
-        print('oi')
+    def sendMail(self, menssagem, destinatario):
         gmail_user = 'trabalhoredes2019@gmail.com'
         gmail_password = 'trabalhoredes'
-        Subject: "Modificações no arquivo"
+        msg = EmailMessage()
+        msg.set_content(menssagem)
+        msg['From'] = gmail_user
+        msg['To'] = destinatario
+        msg['Subject'] = "Modificações no arquivo"
         try:
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.ehlo()
             server.login(gmail_user, gmail_password)
-            server.sendmail(gmail_user, destinatario, menssagem)
+            server.sendmail(gmail_user, destinatario, msg.as_string())
             server.close()
             print("deu certo o envio do email")
         except:
             print('Deu ruim no envio do e-mail')
-
-Mail().sendMail()
